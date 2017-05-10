948332bd2a76d678aaa4455a927b2651770bb25d
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2017-05-09 18:54:29 -0700

    update db

--- config/database.js
+++ config/database.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+require('dotenv').load();
+
+
 var Sequelize = require('sequelize');
 let db = null;
 
 if(process.env.DATABASE_URL){
-  db = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres'
-  });
+  db = new Sequelize(process.env.DATABASE_URL);
   console.log('Connected to db');
 } else {
   db = new Sequelize('whats_happening', 'root', '', {
